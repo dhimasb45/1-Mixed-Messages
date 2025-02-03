2bae6c40293486c224c4ff00e24b56ceee04def1
Dhimas Budiarso <dhimasb45@gmail.com>
2025-02-03 14:29:13 +0700

    Penambahan 3 Array

---
+++ script.js
@@ -0,0 +1,40 @@
+const awalan = [
+  "Jangan pernah lupa bahwa",
+  "Ingatlah selalu",
+  "Setiap hari adalah kesempatan untuk",
+  "Percayalah bahwa",
+  "Mimpi-mimpimu akan terwujud jika",
+  "Kekuatan terbesarmu terletak pada",
+  "Setiap langkah kecilmu membawa",
+  "Kebahagiaan sejati dimulai ketika",
+  "Jangan takut untuk",
+  "Kesuksesan adalah tentang",
+];
+
+const kataKerja = [
+  "melangkah maju",
+  "mencoba hal baru",
+  "belajar dari kegagalan",
+  "berani bermimpi besar",
+  "percaya pada diri sendiri",
+  "menciptakan perubahan",
+  "membangun harapan",
+  "menggapai impian",
+  "menghargai proses",
+  "berjuang tanpa menyerah",
+];
+
+const penutup = [
+  "menuju masa depan yang cerah.",
+  "mencapai tujuanmu.",
+  "menjadi versi terbaik dari dirimu.",
+  "mengubah dunia sekitarmu.",
+  "menciptakan kehidupan yang berarti.",
+  "menemukan kebahagiaan sejati.",
+  "meraih kesuksesan yang kamu impikan.",
+  "memberikan dampak positif bagi orang lain.",
+  "membuka pintu peluang baru.",
+  "menginspirasi banyak orang.",
+];
+
+console.log(awalan[0] + " " + kataKerja[0] + " " + penutup[0]);
